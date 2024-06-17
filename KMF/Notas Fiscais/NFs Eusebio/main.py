24ed83d5be085a94385eb900262757153560c47e
Luiz <luizgusqueiroz@gmail.com>
2024-06-17 17:53:03 -0300

    Modularização do código

--- KMF/Notas Fiscais/NFs Eusebio/main.py
+++ KMF/Notas Fiscais/NFs Eusebio/main.py
@@ -10,86 +10,88 @@ from datetime import datetime
 from PyPDF2 import PdfReader
 from time import sleep
 
-# Inicializa o navegador
-service = Service(ChromeDriverManager().install())
-options = Options()
-options.add_experimental_option("prefs", {
-    "download.default_directory": f'{getcwd()}\\notas',
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True,
-    "profile.default_content_setting_values.automatic_downloads": 1
-})
-nav = webdriver.Chrome(service=service, options=options)
+def main():
+
+    # Inicializa o navegador
+    service = Service(ChromeDriverManager().install())
+    options = Options()
+    options.add_experimental_option("prefs", {
+        "download.default_directory": f'{getcwd()}\\notas',
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True,
+        "profile.default_content_setting_values.automatic_downloads": 1
+    })
+    nav = webdriver.Chrome(service=service, options=options)
 
 
-def espera_aparecer(xpath: str, n: int = 20) -> None:
-    """
-    Espera n segundos até que determinado item esteja na tela.
-    :param xpath: Uma string com o xpath do item que se deseja esperar aperecer.
-    :param n: O total de segundos que se deseja esperar.
-    """
-    for _ in range(n):
-        if nav.find_elements('xpath', xpath):
-            return
-        sleep(1)
-    raise NoSuchElementException(f'{xpath} não encontrado em {n} segundos de espera')
-
-
-def interact(action: str, xpath: str, keys: str = None, n_tries: int = 10) -> None:
-    """
-    Interage com determinado elemento na tela.
-    Primeiro chama a função 'espera_aparecer' para não causar erro de Not Found.
-    :param xpath: Uma string com o xpath do elemento que se deseja esperar aperecer.
-    """
-    actions = ['click', 'write', 'clear']
-    if action not in actions:
-        raise TypeError(f'{action} must be in {actions}')
-    espera_aparecer(xpath)
-    for try_i in range(n_tries):
-        try:
-            # Find the element
-            element = nav.find_element('xpath', xpath)
-            # Choose the right action to do
-            if   action == 'click':
-                element.click()
-            elif action == 'write':
-                element.send_keys(keys)
-            elif action == 'clear':
-                element.clear()
-            return
-        except NoSuchElementException:
-            if try_i == n_tries - 1:
-                print(f'Item com xpath {xpath} não encontrado')
+    def espera_aparecer(xpath: str, n: int = 20) -> None:
+        """
+        Espera n segundos até que determinado item esteja na tela.
+        :param xpath: Uma string com o xpath do item que se deseja esperar aperecer.
+        :param n: O total de segundos que se deseja esperar.
+        """
+        for _ in range(n):
+            if nav.find_elements('xpath', xpath):
+                return
             sleep(1)
-        except ElementNotInteractableException:
-            if try_i == n_tries - 1:
-                print(f'Elemento com xpath {xpath} indisponível ou oculto.')
-            sleep(1)
-        except ElementClickInterceptedException:
-            if try_i == n_tries - 1:
-                print(f'Elemento com xpath {xpath} interceptado ou sobreposto.')
-            sleep(1)
-        except UnexpectedAlertPresentException:
+        raise NoSuchElementException(f'{xpath} não encontrado em {n} segundos de espera')
+
+
+    def interact(action: str, xpath: str, keys: str = None, n_tries: int = 10) -> None:
+        """
+        Interage com determinado elemento na tela.
+        Primeiro chama a função 'espera_aparecer' para não causar erro de Not Found.
+        :param xpath: Uma string com o xpath do elemento que se deseja esperar aperecer.
+        """
+        actions = ['click', 'write', 'clear']
+        if action not in actions:
+            raise TypeError(f'{action} must be in {actions}')
+        espera_aparecer(xpath)
+        for try_i in range(n_tries):
             try:
-                alert = nav.switch_to.alert
-                alert.dismiss()  # Recusa o alerta
-            except NoAlertPresentException:
-                pass
+                # Find the element
+                element = nav.find_element('xpath', xpath)
+                # Choose the right action to do
+                if   action == 'click':
+                    element.click()
+                elif action == 'write':
+                    element.send_keys(keys)
+                elif action == 'clear':
+                    element.clear()
+                return
+            except NoSuchElementException:
+                if try_i == n_tries - 1:
+                    print(f'Item com xpath {xpath} não encontrado')
+                sleep(1)
+            except ElementNotInteractableException:
+                if try_i == n_tries - 1:
+                    print(f'Elemento com xpath {xpath} indisponível ou oculto.')
+                sleep(1)
+            except ElementClickInterceptedException:
+                if try_i == n_tries - 1:
+                    print(f'Elemento com xpath {xpath} interceptado ou sobreposto.')
+                sleep(1)
+            except UnexpectedAlertPresentException:
+                try:
+                    alert = nav.switch_to.alert
+                    alert.dismiss()  # Recusa o alerta
+                except NoAlertPresentException:
+                    pass
 
 
-# Lê as credenciais de acesso
-with open('config.txt', 'r') as file:
-    # Separa o arquivo por linha
-    dados = file.read().split('\n')
-    # Separa cada chave do seu valor
-    dados = [(linha.split('-->')[0], linha.split('-->')[1].strip()) for linha in dados]
-    # Cria um dicionário com os valores lidos
-    dados = {chave: valor for chave, valor in dados}
-    # Converte a data no modelo dd/mm/YYYY para uma variável do tipo datetime
-    data = datetime(int(dados['Data'][6:]), int(dados['Data'][3:5]), int(dados['Data'][:2]))
+    # Lê as credenciais de acesso
+    with open('config.txt', 'r') as file:
+        # Separa o arquivo por linha
+        dados = file.read().split('\n')
+        # Separa cada chave do seu valor
+        dados = [(linha.split('-->')[0], linha.split('-->')[1].strip()) for linha in dados]
+        # Cria um dicionário com os valores lidos
+        dados = {chave: valor for chave, valor in dados}
+        # Converte a data no modelo dd/mm/YYYY para uma variável do tipo datetime
+        data = datetime(int(dados['Data'][6:]), int(dados['Data'][3:5]), int(dados['Data'][:2]))
 
-def run():
+    #------------------------------------
     # Verifica se as pata de destino das notas já existe
     if not path.exists('notas'):
         mkdir('notas')
@@ -168,9 +170,9 @@ def run():
             if not path.exists(f'notas/{empresa}'):
                 mkdir(f'notas/{empresa}')
             # Verifica se o arquivo já existe na pasta.
-            if not path.exists(f'notas/{empresa}/{condominio}-{num_nf}.pdf'):
+            if not path.exists(f'notas/{empresa}/{condominio} - {num_nf} adm.pdf'):
                 # Move o arquivo para a nova pasta
-                rename(nota, f'notas/{empresa}/{condominio}-{num_nf}.pdf')
+                rename(nota, f'notas/{empresa}/{condominio} - {num_nf} adm nf.pdf')
             else: # Apaga a nota
                 remove(nota)
 
@@ -182,4 +184,9 @@ def run():
     nav.quit()
 
 
-run()
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        input()
