2cab43bb0dffd101abf67dd54024b437e0c5b378
Luiz <luizgusqueiroz@gmail.com>
2024-07-23 17:39:57 -0300

    concluida separacao incremental

--- HISEG/Separacao Incremental Parquet/main.py
+++ HISEG/Separacao Incremental Parquet/main.py
@@ -7,11 +7,27 @@ def separa_parquet_incremental(path: str, destino: str, tamanho_pedaco: int = 30
     # Carregar o arquivo Parquet original para um DataFrame
     df_original = pd.read_parquet(path)
 
+    particoes_completas = len(os.listdir(destino))
+    # Verifica se a última partição está incompleta
+    if os.listdir(destino):
+        ultimo_parquet = pd.read_parquet(f'destino/{sorted(os.listdir(destino))[-1]}')
+        if ultimo_parquet.shape != (tamanho_pedaco, 3):
+            n_linhas = ultimo_parquet.shape[0]
+            particoes_completas -= 1
+            # Descarta as linhas desnecessárias
+            df_original = df_original.iloc[tamanho_pedaco*particoes_completas+n_linhas:].reset_index(drop=True)
+            ultimo_parquet = pd.concat([ultimo_parquet, df_original.iloc[:tamanho_pedaco-n_linhas]])
+            nome_arquivo = os.path.join(destino, f'particao_{particoes_completas:04}.parquet')
+            ultimo_parquet.to_parquet(nome_arquivo, index=False, engine='pyarrow')
+            # Descarta as demais linhas do df principal
+            df_original = df_original.iloc[tamanho_pedaco-n_linhas:]
+            particoes_completas += 1
+
     # Dividir o DataFrame original em pedaços menores
     pedacos = [df_original[i:i + tamanho_pedaco] for i in range(0, len(df_original), tamanho_pedaco)]
 
     # Salvar cada pedaço como um arquivo Parquet separado na pasta de destino
-    for idx, pedaco in enumerate(pedacos):
+    for idx, pedaco in enumerate(pedacos, start=particoes_completas):
         nome_arquivo = os.path.join(destino, f'particao_{idx:04}.parquet')
 
         if os.path.exists(nome_arquivo):
@@ -23,8 +39,6 @@ def separa_parquet_incremental(path: str, destino: str, tamanho_pedaco: int = 30
 
 
 if __name__ == '__main__':
-    print(pd.read_parquet.__doc__)
-    exit()
     try:
         separa_parquet_incremental(
             # path=r"C:\Users\ADM\OneDrive - KMF - CONSULTORIA EMPRESARIAL E TREINAMENTOS LTDA - ME\HISEG - Sharepoint\somaseg_alarmes\output.parquet",
