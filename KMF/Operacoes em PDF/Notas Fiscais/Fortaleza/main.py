12f040cfe0bbebaca29cd416f9209948b8e1c0a6
Luiz <luizgusqueiroz@gmail.com>
2024-08-28 14:49:36 -0300

    add identificacao modelo

--- KMF/Operacoes em PDF/Notas Fiscais/Fortaleza/main.py
+++ KMF/Operacoes em PDF/Notas Fiscais/Fortaleza/main.py
@@ -9,11 +9,27 @@ def main() -> None:
             pdf = PdfReader(file_b).pages[0]
             rows = pdf.extract_text().split('\n')
 
-        num_nf = rows[1][:4]
-        for row in rows:
-            if 'Complemento:' in row:
-                nome = row[12:].strip()
-                break
+        if rows[0] == 'Número da':
+            # Modelo 1
+            num_nf = rows[1][:4]
+            for row in rows:
+                if 'Complemento:' in row:
+                    nome = row[12:].strip()
+                    break
+        elif rows[0] == 'Dados do Prestador de Serviços':
+            # modelo 2
+            primeiro = True
+            for i, row in enumerate(rows):
+                if row == 'NFS-e':
+                    num_nf = rows[i+1]
+                if row == 'Razão Social/Nome':
+                    if primeiro:
+                        primeiro = False
+                    else:
+                        nome = rows[i+1]
+                        break
+        else:
+            continue
 
         os.rename(file, f'NF {num_nf} - {nome}.pdf')
 
@@ -23,3 +39,4 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         print(e)
+        input()
