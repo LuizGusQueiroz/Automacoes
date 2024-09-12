b67799673cec3ab2e2c1fefeed5c462e309c18fa
Luiz <luizgusqueiroz@gmail.com>
2024-09-12 12:22:24 -0300

    Add NFs Fortaleza

--- KMF/Operacoes em PDF/_Manipulador de PDF/main.py
+++ KMF/Operacoes em PDF/_Manipulador de PDF/main.py
@@ -689,7 +689,34 @@ def nfs_curitiba()  -> None:  # 31
 
 
 def nfs_fortaleza() -> None:  # 32
-    ...
+    files = [file for file in os.listdir() if '.pdf' in file.lower()]
+    for file in files:
+        with open(file, 'rb') as file_b:
+            pdf = PdfReader(file_b).pages[0]
+            rows = pdf.extract_text().split('\n')
+
+        if rows[0] == 'Número da':
+            # Modelo 1
+            num_nf = rows[1][:4]
+            for row in rows:
+                if 'Complemento:' in row:
+                    nome = row[12:].strip()
+                    break
+        elif rows[0] == 'Dados do Prestador de Serviços':
+            # modelo 2
+            primeiro = True
+            for i, row in enumerate(rows):
+                if row == 'NFS-e':
+                    num_nf = rows[i + 1]
+                if row == 'Razão Social/Nome':
+                    if primeiro:
+                        primeiro = False
+                    else:
+                        nome = rows[i + 1]
+                        break
+        else:
+            continue
+        os.rename(file, f'NF {num_nf} - {nome}.pdf')
 
 
 def nfs_salvador() -> None:  # 33
