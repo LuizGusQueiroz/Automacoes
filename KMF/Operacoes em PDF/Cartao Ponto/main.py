6444813be657b29c56cc68ecc8b8171e4805bd4b
Luiz <luizgusqueiroz@gmail.com>
2024-10-17 16:14:43 -0300

    First commit Cartão de Ponto

---
+++ KMF/Operacoes em PDF/Cartao Ponto/main.py
@@ -0,0 +1,27 @@
+from PyPDF2 import PdfReader, PdfWriter
+from tqdm import tqdm
+import os
+
+
+def main():
+    if not os.path.exists('Arquivos'):
+        os.mkdir('Arquivos')
+    files = [file for file in os.listdir() if '.pdf' in file.lower()]
+    for file in files:
+        with open(file, 'rb') as file_b:
+            pdf = PdfReader(file_b)
+            for page in tqdm(pdf.pages):
+                rows = page.extract_text().split('\n')
+                nome = rows[2]
+                writer = PdfWriter()
+                writer.add_page(page)
+                with open(f'Arquivos/{nome}.pdf', 'wb') as output:
+                    writer.write(output)
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
